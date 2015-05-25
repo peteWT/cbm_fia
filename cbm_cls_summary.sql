0baebd9f070d81b87974d8749b248fef979cdaf2
Peter Tittmann <ptittmann@gmail.com>
2015-05-25 15:36:53 -0700

    added csv summary file for CBM

--- cbm_cls_summary.sql
+++ cbm_cls_summary.sql
@@ -1,35 +1,6 @@
 drop table if exists cbm_cls_summary;
 
 create table cbm_cls_summary as select
-	p.unitcd,
-	c.reservcd,
-	c.fortypcd,
-	c.owngrpcd,
-	case when stdage = 0 then 'AGEID0'
-	     when stdage < 100 then 'AGEID'||ceil(stdage/10)
-	     when stdage >= 100 then 'AGEID10'
-	     else 'err'||stdage
-	end as agerange,
---	'AGEID'||floor(stdage/10)*10 || '_'||floor(stdage/10)*10+9 as agerange,
---	'AGEID'||ceil(stdage/10) as agerange,
-	sum(expns) acres
-from plot p
-left join cond c
-     on (p.cn=c.plt_cn)
-join pop_plot_stratum_assgn a on(p.cn=a.plt_cn)
-join pop_stratum s on(a.stratum_cn=s.cn)
-where p.invyr=2013 and fortypcd >0 and c.condid=1
-group by
-      p.unitcd,
-      c.reservcd,
-      c.fortypcd,
-      c.owngrpcd,
-      agerange
-order by
-      agerange
-;
-
-select
 	sum(case when c.prop_basis='MACR'
 	     then psm.expns * c.condprop_unadj * psm.adj_factor_macr
 	     else psm.expns * c.condprop_unadj * psm.adj_factor_subp
