4ea81e67db9c44c4ad1eeabdad1a91c29ac0989b
Peter Tittmann <ptittmann@gmail.com>
2015-06-01 14:30:52 -0700

    branch 'default' bookmark 'fixes' changed cbm_cls_summary.sql altered query to use join syntax and got the acres closer to the correct estimate changed output/cbm_cls_summary.csv based on updated query

--- cbm_cls_summary.sql
+++ cbm_cls_summary.sql
@@ -14,37 +14,24 @@ create table cbm_cls_summary as select
 	     when stdage >= 200 then 'AGEID999'
 	     else 'NOAGE'
 	end as agerange
-
-FROM cond c,
-     plot p,
-     pop_plot_stratum_assgn ppsa,
-     pop_stratum psm,
-     pop_estn_unit peu,
-     pop_eval pev,
-     pop_eval_typ pet,
-     pop_eval_grp peg,
-     ref_forest_type ftc,
-     ref_forest_type_group fgc,
-     cbm_fgroup fg,
-     owngrpcd ogc,
-     reservcd rcd,
-     ref_unit ru
+FROM cond c
+left join plot p on (c.plt_cn = p.cn)
+left join pop_plot_stratum_assgn ppsa on (ppsa.plt_cn = p.cn)
+left join pop_stratum psm on (ppsa.stratum_cn = psm.cn)
+left join pop_estn_unit peu on (peu.cn = psm.estn_unit_cn)
+left join pop_eval pev on (pev.cn = peu.eval_cn)
+left join pop_eval_typ pet on (pev.cn = pet.eval_cn)
+left join pop_eval_grp peg on(pet.eval_grp_cn = peg.cn)
+left join reservcd rcd using(reservcd)
+left join owngrpcd ogc using(owngrpcd)
+left join ref_forest_type ftc on (c.fortypcd=ftc.value)
+left join cbm_fgroup fg on (ftc.typgrpcd=fg.typgrpcd)
+left join ref_unit ru on p.unitcd=ru.value and p.statecd=ru.statecd
 WHERE p.cn = c.plt_cn
 AND pet.eval_typ = 'EXPCURR'
 AND c.cond_status_cd = 1
 AND c.siteclcd IN (1, 2, 3, 4, 5, 6)
-AND ppsa.plt_cn = p.cn
-AND ppsa.stratum_cn = psm.cn
-AND peu.cn = psm.estn_unit_cn
-AND pev.cn = peu.eval_cn
-AND pev.cn = pet.eval_cn
-AND pet.eval_grp_cn = peg.cn
 AND peg.eval_grp = 62013
-and c.fortypcd=ftc.value
-and ftc.typgrpcd=fg.typgrpcd
-and c.reservcd=rcd.reservcd
-and c.owngrpcd=ogc.owngrpcd
-and p.unitcd=ru.value
-and p.statecd=ru.statecd
-group by rcd.cbm_cd, fg.cbm_spcd, ru.meaning, ogc.cbm_ogcd, agerange;
+group by reserve_group, owner_group, forest_group, ref_unit, agerange;
+
 
